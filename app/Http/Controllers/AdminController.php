eb620782cf5e1f52bf9093a30f1d3c3c1c5b80df
Ghyghi <nagasaroghislaine3@gmail.com>
2024-10-01 13:55:38 +0200

    added admin dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function admin_dashboard(){
-        return view('adminDashboard');
+        $count = User::where('isAdmin', '!=', 1)->count();
+        $records = User::where('isAdmin', '!=', 1)->get();
+        $admin_count = User::where('isAdmin', '=', 1)->count();
+        $admin_records = User::where('isAdmin', '=', 1)->get();
+        return view('adminDashboard', ['count'=>$count, 'records'=> $records, 'admin_count' => $admin_count, 'admin_records'=>$admin_records]);
     }
 }
--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -1,3 +1,16 @@
 <x-layout>
     <h1>Admin Dashboard</h1>
+    <p>Welcome to the admin dashboard, {{ auth()->user()->name }}!</p>
+    <p>The system has {{ $count }} non-admin users;</p>
+    <ul>
+        @foreach ($records as $record)
+            <li>{{ $record->name }}</li>
+        @endforeach
+    </ul>
+    <p>The system has {{ $admin_count }} admin user(s);</p>
+    <ul>
+        @foreach ($admin_records as $records)
+            <li>{{ $records->name }}</li>
+        @endforeach
+    </ul>
 </x-layout>
